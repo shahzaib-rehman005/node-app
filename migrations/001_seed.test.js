417343960ae5afa4410f776831606925892af5f5
shahzaib rehman <nk780877@gmail.com>
2026-07-23 19:54:40 +0500

    move migration under db/

--- migrations/001_seed.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert');
-const { applyDiscount } = require('./001_seed');
-
-test('applyDiscount subtracts the discount', () => {
-  // 100 with 20%% off must be 80
-  assert.strictEqual(applyDiscount(100, 20), 80);
-});
