789387bae3e4574c0be04768b9906f3f07a32bcb
shahzaib rehman <nk780877@gmail.com>
2026-07-23 19:54:38 +0500

    move migration under db/

--- migrations/001_seed.js
+++
@@ -1,7 +0,0 @@
-// Migration helper: compute price after applying a discount percentage.
-function applyDiscount(price, percent) {
-  // BUG: adds the discount instead of subtracting it
-  return price + (price * percent) / 100;
-}
-
-module.exports = { applyDiscount };
